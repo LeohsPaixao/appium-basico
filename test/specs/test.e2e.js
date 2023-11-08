baa406c1036833fde138697f5e67624071da69ec
Leonardo Pa[Dixão <leonardohspaixao@gmail.com>
2023-11-08 18:48:11 -0300

    (Colocado o App e mudando algumas configuracoes)

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -3,11 +3,6 @@ const { expect, $ } = require('@wdio/globals')
 describe('My Login application', () => {
     it('should login with valid credentials', async () => {
 
-        await $('//android.widget.EditText[@text="E-mail"]').setValue('leonardohspaixao@gmail.com')
-        await $('//android.widget.EditText[@text="Senha"]').setValue('123456')
-        await $('//android.widget.TextView[@text="Entrar"]').click()
-
-        await expect($('android.view.ViewGroup')).toBeExisting()
     })
 })
 
