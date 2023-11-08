ecdeb85a5619051bc02b6957fd6da2a37b03d45c
Leonardo Pa[Dixão <leonardohspaixao@gmail.com>
2023-11-08 13:55:23 -0300

    (Novo projeto com appiium)

---
+++ test/specs/test.e2e.js
@@ -0,0 +1,18 @@
+const { expect, $ } = require('@wdio/globals')
+
+describe('My Login application', () => {
+    it('should login with valid credentials', async () => {
+
+        await $('//android.widget.EditText[@text="E-mail"]').setValue('leonardohspaixao@gmail.com')
+        await $('//android.widget.EditText[@text="Senha"]').setValue('123456')
+        await $('//android.widget.TextView[@text="Entrar"]').click()
+
+        await expect($('android.view.ViewGroup')).toBeExisting()
+    })
+
+    it.only('should deslooger the account', async () => {
+        const button = [35,124][151,240]
+        await driver.positionClick(button, 0)
+    })
+})
+
