f9c59e1afd9d1f3ba318781e7f115b1e4068c63e
Leonardo Pa[Dixão <leonardohspaixao@gmail.com>
2023-11-10 19:10:41 -0300

    (Teste simples de click)

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -1,8 +1,10 @@
 const { expect, $ } = require('@wdio/globals')
 
 describe('WebdriverIo', () => {
-    it('Should able to do login', () => {
+    it('Should able to click login icon', async () => {
+        const LOGIN_ICON = '//*[@content-desc="Login"]'
 
+        await $(LOGIN_ICON).click()
     });
 })
 
