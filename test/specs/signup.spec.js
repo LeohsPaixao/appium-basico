f511cdbc779b417df2c8e7d19d9c6a8bc5106249
Leonardo Pa[Dixão <leonardohspaixao@gmail.com>
2023-11-10 22:37:49 -0300

    (test screen login)

---
+++ test/specs/signup.spec.js
@@ -0,0 +1,58 @@
+describe('-> Sign up', () => {
+    beforeEach( async () => {
+        await $('~Login').click()
+        await $('//android.widget.TextView[@text="Sign up"]').click()
+        await expect(
+            $('~input-repeat-password')
+        ).toBeExisting()
+    })
+
+    after( async () => {
+        await driver.pause(1000)
+        await driver.close()
+    })
+
+    it('Should not be able to sign in with email invalid', async () => {
+        await $('~input-email').addValue('a@')
+        await $('~input-password').addValue('12345678')
+        await $('~input-repeat-password').addValue('12345678')
+
+        await expect(
+            $('//android.widget.TextView[@text="Please enter a valid email address"]')
+        ).toHaveText('Please enter a valid email address')
+    })
+
+    it('Should not be able to sign in with password invalid', async () => {
+        await $('~input-email').addValue('a@example.com')
+        await $('~input-password').addValue('123456')
+        await $('~input-repeat-password').addValue('12345678')
+
+        await expect(
+            $('//android.widget.TextView[@text="Please enter at least 8 characters"]')
+        ).toHaveText('Please enter at least 8 characters')
+
+        await expect(
+            $('//android.widget.TextView[@text="Please enter the same password"]')
+        ).toHaveText('Please enter the same password')
+    })
+
+    it('Should not be able to sign in with password confirm invalid', async () => {
+        await $('~input-email').addValue('a@example.com')
+        await $('~input-password').addValue('12345678')
+        await $('~input-repeat-password').addValue('123456')
+
+        await expect(
+            $('//android.widget.TextView[@text="Please enter the same password"]')
+        ).toHaveText('Please enter the same password')
+    })
+
+    it('Should be able to sign in', async () => {
+        await $('~input-email').addValue('a@example.com')
+        await $('~input-password').addValue('12345678')
+        await $('~input-repeat-password').addValue('12345678')
+
+        await expect(
+            $('//android.widget.FrameLayout[@resource-id="android:id/content"]')
+        ).toBeExisting()
+    })
+})
