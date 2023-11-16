30182a715505cd744b7af9a7bcd569c8f0860b4d
Leonardo Paixão <42840902+LeohsPaixao@users.noreply.github.com>
2023-11-16 15:17:55 -0300

    Update swipe.spec.js

--- test/specs/swipe.spec.js
+++ test/specs/swipe.spec.js
@@ -1,11 +1,38 @@
-describe('Swipe', () => {
-    beforeEach( async () => {
+describe('-> Swipe', () => {
+    beforeEach(async () => {
         await $('~Swipe').click();
         await expect($('//android.widget.TextView[@text="Swipe horizontal"]')).toHaveText('Swipe horizontal');
     });
 
-    it('Should be able to swipe for next item', () => {
-        const { height, width } = driver.getWindowSize();
-        console.log(height, width);
-    })
+    after(async () => {
+        await driver.pause(1000);
+    });
+
+    it.skipe('Should be able to swipe horizontally', async () => {
+        // Elemento alvo para verificar após o swipe
+        const targetElement = await $('//android.widget.TextView[@text="GREAT COMMUNITY"]');
+
+        // Obtenha a posição inicial e final para o swipe
+        const startX = 0.8; // Porcentagem da largura da tela
+        const endX = 0.2; // Porcentagem da largura da tela
+        const y = 0.5; // Porcentagem da altura da tela
+
+        const { height, width } = await driver.getWindowSize();
+
+        // Calcula as coordenadas reais
+        const startCoords = { x: width * startX, y: height * y };
+        const endCoords = { x: width * endX, y: height * y };
+
+        // Realiza o swipe
+        await driver.touchPerform([
+            { action: 'press', options: startCoords },
+            { action: 'wait', options: { ms: 1000 } },
+            { action: 'moveTo', options: endCoords },
+            { action: 'release', options: {} },
+        ]);
+
+        // Verifica se o elemento alvo está visível após o swipe
+        const isTargetVisible = await targetElement.isDisplayed();
+        assert.isTrue(isTargetVisible, 'O swipe horizontal falhou ou o elemento alvo não está visível após o swipe');
+    });
 })
