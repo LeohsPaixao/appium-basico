8ff57a061f92abf0a8c8f0178f83e144a542bb0f
Leonardo Pa[Dixão <leonardohspaixao@gmail.com>
2023-11-19 02:41:22 -0300

    (Preparacao para testes no webview)

---
+++ test/specs/webview.spec.js
@@ -0,0 +1,12 @@
+describe('-> Webview', () => {
+    beforeEach( async () => {
+        await $('~Webview').click();
+        await expect($('id', '__docusaurus')).toBeDisplayed();
+    })
+
+    it('Should')
+
+    it('Should')
+
+    it('Should')
+})
