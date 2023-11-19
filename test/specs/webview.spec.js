6f656de1d2eca6c12f288b449e84518eeb37a782
Leonardo Pa[Dixão <leonardohspaixao@gmail.com>
2023-11-19 17:39:10 -0300

    (Teste na parte de Webview da aplicacao)

--- test/specs/webview.spec.js
+++ test/specs/webview.spec.js
@@ -1,12 +1,81 @@
 describe('-> Webview', () => {
-    beforeEach( async () => {
+    before( async () => {
         await $('~Webview').click();
-        await expect($('id', '__docusaurus')).toBeDisplayed();
+        await driver.pause(5000); // Como é Webview, leva um tempo para carregar a tela
     })
 
-    it('Should')
+    after(() => {
+        driver.pause(1000);
+    })
 
-    it('Should')
+    it('Should be able to navigate for Get Started and return', async () => {
+        // Coordenadas iniciais e finais para o swipe
+        const startX = 50;
+        const startY = 2500;
+        const endX = 50;
+        const endY = 1500;
 
-    it('Should')
+        // Realizar o swipe para cima usando performActions
+        await driver.performActions([
+            {
+                type: 'pointer',
+                id: 'finger1',
+                parameters: { pointerType: 'touch' },
+                actions: [
+                    { type: 'pointerMove', duration: 500, x: startX, y: startY },
+                    { type: 'pointerDown', button: 0 },
+                    { type: 'pointerMove', duration: 500, x: endX, y: endY },
+                    { type: 'pointerUp', button: 0 },
+                ],
+            },
+        ]);
+
+        // Garantir que a ação de swipe seja concluída antes de prosseguir
+        await driver.pause(2500);
+
+        // Pagina do Get Started
+        const button_GetStarted = await $('~Get Started');
+        await button_GetStarted.click();
+
+        const Breadcrumbs = await $('//android.view.View[@text="Breadcrumbs"]');
+        await expect(Breadcrumbs).toBeDisplayed();
+
+        const home = await $('~WebdriverIO');
+        await home.click();
+
+        const home_screen = await $('//android.view.View[@resource-id="__docusaurus"]');
+        await expect(home_screen).toBeDisplayed();
+    })
+
+    it('Should be able to chance the colr white for black ', async () => {
+        const navigation_bar = await $('//android.widget.Button[@text="Toggle navigation bar"]');
+        await navigation_bar.click();
+
+        driver.pause(3000);
+
+        const modeColor = await $('//android.widget.Button[@text="Switch between dark and light mode (currently light mode)"]');
+        await modeColor.click();
+
+        await expect(modeColor).toHaveTextContaining('dark mode');
+
+        const Cnavigation_bar = await $('//android.widget.Button[@text="Close navigation bar"]');
+        await Cnavigation_bar.click();
+    })
+
+    it('Should be able to search something', async () => {
+        const button_Search = await $('//android.widget.Button[@text="Search"]');
+        await button_Search.click();
+
+        const search = await $('//android.widget.EditText[@resource-id="docsearch-input"]');
+        await expect(search).toBeDisplayed();
+
+        await search.addValue('Appium');
+
+        const protocols = await $('~Appium');
+        await protocols.click();
+
+        const appium_screen = await $('//android.widget.TextView[@text="Appium"]');
+        await expect(appium_screen).toBeDisplayed();
+
+    })
 })
