1894fe029f3df665fcf483cda031c513c43fdc1f
Leonardo Pa[Dixão <leonardohspaixao@gmail.com>
2023-11-21 17:26:19 -0300

    (Teste CI/CD #18)

--- scripts/appium-server.sh
+++ scripts/appium-server.sh
@@ -6,5 +6,3 @@ echo "Install and Running Appium Server as a Background process"
 sudo npm install -g appium --unsafe-perm=true --allow-root
 sudo appium -v
 sudo appium --config .appiumrc.json &>/dev/null &
-
-sudo gpasswd -a $USER kvm
--- scripts/appium-tests.sh
+++ scripts/appium-tests.sh
@@ -3,4 +3,5 @@ set -ex
 
 echo "Install and Running Appium Server as a Background process"
 
+sudo gpasswd -a $USER kvm
 sudo npm test
