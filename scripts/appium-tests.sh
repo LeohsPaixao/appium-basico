d5a981ea0c2dc19a0b4517e2e86140c15acd22e2
Leonardo Pa[Dixão <leonardohspaixao@gmail.com>
2023-11-23 14:11:03 -0300

    (Teste CI/CD #41)

--- scripts/appium-tests.sh
+++ scripts/appium-tests.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -ex
 
-echo "Install and Running Appium Server as a Background process"
+echo "Put user in group KVM and run tests"
 
+sudo usermod -aG kvm runner
 sudo gpasswd -a $USER kvm
+sudo newgrp kvm
 sudo npm test
