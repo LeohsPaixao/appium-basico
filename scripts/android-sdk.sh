c3c9ae6d44165f007bc69d1073736710a27e162f
Leonardo Pa[Dixão <leonardohspaixao@gmail.com>
2023-11-24 17:51:54 -0300

    (Teste CI/CD com ENV #14)

--- scripts/android-sdk.sh
+++ scripts/android-sdk.sh
@@ -3,6 +3,8 @@ set -ex
 
 echo "Android SDK Configuration"
 
+brew install android-sdk
+
 mkdir -p $HOME/android-sdk
 wget -q https://dl.google.com/android/repository/commandlinetools-mac-6609375_latest.zip -O android-sdk.zip
 unzip -qq android-sdk.zip -d $HOME/android-sdk
