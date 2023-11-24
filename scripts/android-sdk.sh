e56e2eaf11451ceb6ce5357e74ab36b8cb0b4274
Leonardo Pa[Dixão <leonardohspaixao@gmail.com>
2023-11-24 17:21:58 -0300

    (Teste CI/CD com ENV #9)

---
+++ scripts/android-sdk.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -ex
+
+echo "Android SDK Configuration"
+
+sudo apt update
+sudo apt install -y openjdk-11-jre-headless
+
+export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+export PATH=$PATH:$JAVA_HOME/bin
+
+mkdir -p $HOME/android-sdk
+wget -q https://dl.google.com/android/repository/commandlinetools-mac-6609375_latest.zip -O android-sdk.zip
+unzip -qq android-sdk.zip -d $HOME/android-sdk
+
+export ANDROID_HOME=$HOME/android-sdk
+export ANDROID_SDK_ROOT=$HOME/android-sdk
+export PATH=$PATH:$ANDROID_HOME/cmdline-tools/bin:$ANDROID_HOME/platform-tools
+
+echo "export PATH=$PATH:$ANDROID_HOME/cmdline-tools/bin:$ANDROID_HOME/platform-tools" >> $HOME/.bashrc
