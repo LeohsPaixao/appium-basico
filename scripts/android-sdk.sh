206189b26cc039f5b6fc8a7a89ec90f4c00d4376
Leonardo Pa[Dixão <leonardohspaixao@gmail.com>
2023-11-24 17:37:52 -0300

    (Teste CI/CD com ENV #13)

--- scripts/android-sdk.sh
+++ scripts/android-sdk.sh
@@ -3,12 +3,6 @@ set -ex
 
 echo "Android SDK Configuration"
 
-sudo brew update
-sudo brew install -y openjdk-11-jre-headless
-
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
-export PATH=$PATH:$JAVA_HOME/bin
-
 mkdir -p $HOME/android-sdk
 wget -q https://dl.google.com/android/repository/commandlinetools-mac-6609375_latest.zip -O android-sdk.zip
 unzip -qq android-sdk.zip -d $HOME/android-sdk
