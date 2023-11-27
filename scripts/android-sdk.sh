2652ef7aa5f56c7dde3d1a4c360a3c9d0923c086
Leonardo Pa[Dixão <leonardohspaixao@gmail.com>
2023-11-27 15:52:23 -0300

    (Teste CI/CD com ENV #24)

--- scripts/android-sdk.sh
+++ scripts/android-sdk.sh
@@ -3,8 +3,6 @@ set -ex
 
 echo "Android SDK Configuration"
 
-brew update
-
 mkdir -p $RUNNER_WORKSPACE/android-sdk
 wget -q https://dl.google.com/android/repository/commandlinetools-mac-6609375_latest.zip -O android-sdk.zip
 unzip -qq android-sdk.zip -d $RUNNER_WORKSPACE/android-sdk
@@ -18,5 +16,8 @@ echo "export ANDROID_HOME=$RUNNER_WORKSPACE/android-sdk" >> $RUNNER_WORKSPACE/.b
 echo "export ANDROID_SDK_ROOT=$RUNNER_WORKSPACE/android-sdk" >> $RUNNER_WORKSPACE/.bash_profile
 echo "export PATH=$PATH:$ANDROID_HOME/cmdline-tools/bin:$ANDROID_HOME/platform-tools" >> $RUNNER_WORKSPACE/.bash_profile
 
-# Carrega o profile
+# Garante que as variáveis sejam carregadas no atual ambiente
 source $RUNNER_WORKSPACE/.bash_profile
+
+# Configura o PATH para garantir que as ferramentas do Android estejam disponíveis
+export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin
--- scripts/appium-server.sh
+++ scripts/appium-server.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 set -ex
 
+# Tratar erros
+trap 'echo "Erro na execução do script"; exit 1' ERR
+
 echo "Install and Running Appium Server as a Background process"
 
-sudo npm install -g appium --unsafe-perm=true --allow-root
-sudo appium -v
-sudo appium --config .appiumrc.json &>/dev/null &
+# Instalar Appium globalmente usando $RUNNER_WORKSPACE
+npm install -g appium --unsafe-perm=true --allow-root --prefix $RUNNER_WORKSPACE
+
+# Verificar a versão do Appium instalado
+appium -v
+
+# Executar Appium em segundo plano
+appium --config .appiumrc.json &>/dev/null &
