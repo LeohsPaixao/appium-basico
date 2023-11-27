7d40323861cc2dd10893af352a543516e6bd96fd
Leonardo Pa[Dixão <leonardohspaixao@gmail.com>
2023-11-27 15:00:33 -0300

    (Teste CI/CD com ENV #22)

--- scripts/shell.sh
+++
@@ -1,7 +0,0 @@
-if [ "$SHELL" = "/bin/bash" ]; then
-    echo "Está rodando no Bash"
-elif [ "$SHELL" = "/bin/zsh" ]; then
-    echo "Está rodando no Zsh"
-else
-    echo "Shell não reconhecido: $SHELL"
-fi
