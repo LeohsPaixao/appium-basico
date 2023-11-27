c78fa80e84c80d8c5e8ed1008e78cd1285f91904
Leonardo Pa[Dixão <leonardohspaixao@gmail.com>
2023-11-27 14:05:12 -0300

    (Teste CI/CD com ENV #18)

--- scripts/android-sdk.sh
+++ scripts/android-sdk.sh
@@ -4,12 +4,7 @@ set -ex
 echo "Android SDK Configuration"
 
 brew update
-brew install --cask homebrew/cask-versions/temurin8
 brew install android-sdk
-brew install -y openjdk-8-jre-headless
-
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-export PATH=$PATH:$JAVA_HOME/bin
 
 mkdir -p $HOME/android-sdk
 wget -q https://dl.google.com/android/repository/commandlinetools-mac-6609375_latest.zip -O android-sdk.zip
