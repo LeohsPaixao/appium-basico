89e6787e00c1393a4c97e884fc523e8e85bdac69
Leonardo Pa[Dixão <leonardohspaixao@gmail.com>
2023-11-28 20:21:56 -0300

    (Altera config para funcionar em varios apps)

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,4 +1,4 @@
-const demoApp = {
+const main = {
     runner: "local",
     port: 4723,
     logLevel: "info",
@@ -20,71 +20,77 @@ const demoApp = {
             disableWebdriverScreenshotsReporting: false,
         },
     },
-    specs: [
-        "./test/specs/**/*.js"
-    ],
-    capabilities: [{
-
-        "platformName": "Android",
-        "appium:platformVersion": "13",
-        "appium:deviceName": "AppiumAVD",
-        "appium:deviceOrientation": "portrait",
-        "appium:automationName": "UiAutomator2",
-        "appium:appPackage": "com.wdiodemoapp",
-        "appium:appActivity": "com.wdiodemoapp.MainActivity",
-        "appium:noReset": false,
-        "appium:autoLaunch": true,
-        "appium:newCommandTimeout": 240,
-    }],
     mochaOpts: {
         ui: "bdd",
-        timeout: 600000
-    },
-}
-
-const apiDemos = {
-    runner: "local",
-    port: 4723,
-    logLevel: "info",
-    framework: "mocha",
-    path: "/wd/hub",
-    bail: 0,
-    baseUrl: "",
-    waitforTimeout: 60000,
-    connectionRetryTimeout: 120000,
-    connectionRetryCount: 3,
-    maxInstances: 1,
-    exclude: [],
-    services: ["appium"],
-    reporters: ["allure"],
-    reporterOptions: {
-        allure: {
-            outputDir: "./allure-results",
-            disableWebdriverStepsReporting: true,
-            disableWebdriverScreenshotsReporting: false,
-        },
-    },
-    specs: [
-        "./test/specs/**/*.js"
-    ],
-    capabilities: [{
-
-        "platformName": "Android",
-        "appium:platformVersion": "13",
-        "appium:deviceName": "AppiumAVD",
-        "appium:deviceOrientation": "portrait",
-        "appium:automationName": "UiAutomator2",
-        "appium:app": "./apps/ApiDemos-debug.apk",
-        "appium:appPackage": "io.appium.android.apis",
-        "appium:appActivity": ".ApiDemos",
-        "appium:noReset": false,
-        "appium:autoLaunch": true,
-        "appium:newCommandTimeout": 240,
-    }],
-    mochaOpts: {
-        ui: "bdd",
-        timeout: 600000
+        timeout: 600000,
     },
 };
 
-exports.config = process.env.APP_ENV === "demoApp" ? demoApp : apiDemos;
+const demoApp = {
+    ...main,
+    capabilities: [
+        {
+            platformName: "Android",
+            "appium:platformVersion": "13",
+            "appium:deviceName": "AppiumAVD",
+            "appium:deviceOrientation": "portrait",
+            "appium:automationName": "UiAutomator2",
+            "appium:appPackage": "com.wdiodemoapp",
+            "appium:appActivity": "com.wdiodemoapp.MainActivity",
+            "appium:noReset": false,
+            "appium:autoLaunch": true,
+            "appium:newCommandTimeout": 240,
+        },
+    ],
+
+    specs: ["./test/specs/NativeDemoApp/*.js"],
+};
+
+const apiDemos = {
+    ...main,
+    capabilities: [
+        {
+            platformName: "Android",
+            "appium:platformVersion": "13",
+            "appium:deviceName": "AppiumAVD",
+            "appium:deviceOrientation": "portrait",
+            "appium:automationName": "UiAutomator2",
+            "appium:app": "./apps/product_registration.apk",
+            "appium:appPackage": "br.com.pztec.estoque",
+            "appium:appActivity": "br.com.pztec.estoque.Inicio",
+            "appium:noReset": false,
+            "appium:autoLaunch": true,
+            "appium:newCommandTimeout": 240,
+        },
+    ],
+
+    specs: ["./test/specs/productApp/*.js"],
+};
+
+const solarMarket = {
+    ...main,
+    capabilities: [
+        {
+            platformName: "Android",
+            "appium:platformVersion": "13",
+            "appium:deviceName": "AppiumAVD",
+            "appium:deviceOrientation": "portrait",
+            "appium:automationName": "UiAutomator2",
+            "appium:app": "./apps/app-debug.apk",
+            "appium:appPackage": "com.solarviewbusinessapp",
+            "appium:appActivity": "com.solarviewbusinessapp.MainActivity",
+            "appium:noReset": false,
+            "appium:autoLaunch": true,
+            "appium:newCommandTimeout": 240,
+        },
+    ],
+
+    specs: ["./test/specs/solarMarket/*.js"],
+};
+
+exports.config =
+    process.env.APP_ENV === "demoApp"
+        ? demoApp
+        : process.env.APP_ENV === "apiDemos"
+        ? apiDemos
+        : solarMarket;
