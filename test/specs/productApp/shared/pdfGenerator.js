29ebafe8275c8bbdcbf1c13528247ef5fafa4b23
Leonardo Pa[Dixão <leonardohspaixao@gmail.com>
2023-12-03 19:24:11 -0300

    (Cria a funcao para gerar pdf no App Product)

--- test/specs/productApp/shared/pdfGenerator.js
+++ test/specs/productApp/shared/pdfGenerator.js
@@ -1,7 +1,6 @@
 const pdfGenerator = async () => {
-    await $("id=br.com.pztec.estoque:id/inventario").click();
     await $("id=br.com.pztec.estoque:id/btn_gerar").click();
-    await expect($("id=br.com.pztec.estoque:id/datafile")).toHaveTextContaining("inventory.pdf generated in");
+    await expect($("id=br.com.pztec.estoque:id/datafile")).toHaveTextContaining("generated in");
 }
 
 module.exports = { pdfGenerator }
