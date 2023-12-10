d99ac0849cc18feac61eb9de9f79ecda96a13c04
Leonardo Pa[Dixão <leonardohspaixao@gmail.com>
2023-12-10 15:07:55 -0300

    (Teste CI Novo #15)

--- scripts/emulator.sh
+++ scripts/emulator.sh
@@ -9,11 +9,8 @@ sdkmanager --update
 # Instalação de pacotes adicionais (ajuste conforme necessário)
 sdkmanager --install "platforms;android-31" "build-tools;31.0.0" "extras;google;m2repository" "extras;android;m2repository" "system-images;android-31;default;x86_64"
 
-# Verifica o pacote da imagem do sistema
-sdkmanager --list
-
 # Criação do AVD (Android Virtual Device)
-echo "no" | avdmanager --verbose create avd --force --name testAVD --package 'system-images;android-31;default;x86_64' --tag default --abi x86_64
+echo "no" | avdmanager --verbose create avd --force --name testAVD --package 'system-images;android-31;default;x86_64' --tag google_apis --abi x86_64
 
 # Verifica se o AVD foi criado com sucesso
 avdmanager list avd
