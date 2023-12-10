6d47c9699f43d94cb57d9d07476a9a9ff2c0129a
Leonardo Pa[Dixão <leonardohspaixao@gmail.com>
2023-12-10 14:18:54 -0300

    (Teste CI Novo 09)

--- scripts/emulator.sh
+++ scripts/emulator.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -ex
 
 echo "Configuração do Emulador"
 
@@ -19,6 +18,6 @@ echo 'y' | sdkmanager --install "system-images;android-30;google_apis;x86_64"
 echo 'no' | avdmanager --verbose create avd --force --name test --package "system-images;android-30;google_apis;x86_64" --abi google_apis/x86_64
 
 # Inicializa o emulador em segundo plano
-emulator -avd test -no-window -no-audio -no-boot-anim -no-jni -camera-back none -camera-front none -selinux permissive -qemu -m 4096 &
+$ANDROID_HOME/emulator/emulator -avd test -no-window -no-audio -no-boot-anim -no-jni -camera-back none -camera-front none -selinux permissive -qemu -m 4096 &
 
 echo "Emulador configurado com sucesso!"
