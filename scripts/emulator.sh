d7f219595eef9d64edf13238799bae7de9d7ac81
Leonardo Pa[Dixão <leonardohspaixao@gmail.com>
2023-12-10 16:13:56 -0300

    (Teste CI Novo #25)

--- scripts/emulator.sh
+++ scripts/emulator.sh
@@ -7,6 +7,12 @@ set -x
 
 echo "Configuração do Emulador"
 
+# Coloca no grupo KVM
+sudo groupadd -r kvm
+sudo gpasswd -a $USER kvm
+sudo gpasswd -a runner kvm
+ls -la /etc/group
+
 # Definir o diretório AVD para evitar problemas com o caminho
 export ANDROID_AVD_HOME=$HOME/.config/.android/avd
 
@@ -14,21 +20,22 @@ export ANDROID_AVD_HOME=$HOME/.config/.android/avd
 sdkmanager --update
 
 # Instalação de pacotes adicionais (ajuste conforme necessário)
-sdkmanager --install "platforms;android-31" "build-tools;31.0.0" "extras;google;m2repository" "extras;android;m2repository" "system-images;android-31;google_apis;x86_64"
+sdkmanager --install "platforms;android-31" "build-tools;31.0.0" "extras;google;m2repository" "extras;android;m2repository" "system-images;android-31;google_apis;x86"
 
 # Criação do AVD (Android Virtual Device)
-echo "no" | avdmanager --verbose create avd --force --name testAVD --package 'system-images;android-31;google_apis;x86_64' --tag google_apis --abi x86_64
+echo "no" | avdmanager --verbose create avd --force --name testAVD --package 'system-images;android-31;google_apis;x86' --tag google_apis --abi x86
 
 # Verifica se o AVD foi criado com sucesso
-avdmanager list avd
-
-# Aguardar para a criação do Emulador
-sleep 6
+if [ -d "$ANDROID_AVD_HOME/testAVD.avd" ]; then
+  echo "AVD criado com sucesso em $ANDROID_AVD_HOME"
+else
+  echo "Erro: Falha ao criar o AVD em $ANDROID_AVD_HOME"
+  exit 1
+fi
 
 # Inicializa o emulador em segundo plano
-$ANDROID_HOME/emulator/emulator -avd testAVD -no-window -no-audio -no-boot-anim -no-jni -camera-back none -camera-front none -selinux permissive -qemu -m 4096 &
+$ANDROID_HOME/emulator/emulator -avd testAVD -no-accel -no-window -no-audio -no-boot-anim -no-jni -camera-back none -camera-front none -selinux permissive -qemu -m 4096 &
 
-# Adicionar logs detalhados
 echo "Aguardando a inicialização completa do emulador"
 until adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done'; do
     sleep 5
