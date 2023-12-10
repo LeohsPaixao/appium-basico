0100a1edbd64f6ca2e0764739af9d70408ca0587
Leonardo Pa[Dixão <leonardohspaixao@gmail.com>
2023-12-10 14:55:15 -0300

    (Teste CI Novo #14)

--- scripts/emulator.sh
+++ scripts/emulator.sh
@@ -7,20 +7,23 @@ echo "Configuração do Emulador"
 sdkmanager --update
 
 # Instalação de pacotes adicionais (ajuste conforme necessário)
-sdkmanager --install "platforms;android-31" "build-tools;31.0.0" "extras;google;m2repository" "extras;android;m2repository" "emulator"
+sdkmanager --install "platforms;android-31" "build-tools;31.0.0" "extras;google;m2repository" "extras;android;m2repository" "system-images;android-31;default;x86_64"
+
+# Verifica o pacote da imagem do sistema
+sdkmanager --list
 
 # Criação do AVD (Android Virtual Device)
-echo "no" | avdmanager --verbose create avd --force --name testAVD --package "system-images;android-31;default;x86_64" --abi google_apis/x86_64
+echo "no" | avdmanager --verbose create avd --force --name testAVD --package 'system-images;android-31;default;x86_64' --tag default --abi x86_64
 
 # Verifica se o AVD foi criado com sucesso
 avdmanager list avd
 
 # Inicializa o emulador em segundo plano
-$ANDROID_HOME/emulator/emulator -avd test -no-window -no-audio -no-boot-anim -no-jni -camera-back none -camera-front none -selinux permissive -qemu -m 4096 &
+$ANDROID_HOME/emulator/emulator -avd testAVD -no-window -no-audio -no-boot-anim -no-jni -camera-back none -camera-front none -selinux permissive -qemu -m 4096 &
 
 # Aguarda até que o emulador esteja completamente inicializado
 until adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done'; do
     sleep 5
 done
 
-echo "Emulador configurado e aberto com sucesso!"
+echo "Emulador configurado com sucesso!"
